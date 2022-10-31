cae3eb5a6ace754153c7d92aa16c8b31bb55e9c4
Benxd <benjaminchoi00@gmail.com>
2022-11-01 00:37:58 +0800

    aerial add default config

--- lua/main/aerial.lua
+++ lua/main/aerial.lua
@@ -6,18 +6,78 @@ end
 
 local config = {
     attach_mode = "global",
-    backends = { "lsp", "treesitter", "markdown" },
+    backends = { "lsp", "treesitter", "markdown", "man" },
     layout = {
-        min_width = 25,
+        -- These control the width of the aerial window.
+        -- They can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+        -- min_width and max_width can be a list of mixed types.
+        -- max_width = {40, 0.2} means "the lesser of 40 columns or 20% of total"
+        max_width = { 40, 0.2 },
+        width = nil,
+        min_width = 10,
+
+        -- key-value pairs of window-local options for aerial window (e.g. winhl)
+        win_opts = {},
+
+        -- Determines the default direction to open the aerial window. The 'prefer'
+        -- options will open the window in the other direction *if* there is a
+        -- different buffer in the way of the preferred direction
+        -- Enum: prefer_right, prefer_left, right, left, float
+        default_direction = "prefer_right",
+
+        -- Determines where the aerial window will be opened
+        --   edge   - open aerial at the far right/left of the editor
+        --   window - open aerial to the right/left of the current window
+        placement = "window",
     },
+
+
+    -- Show box drawing characters for the tree hierarchy
     show_guides = true,
+
     filter_kind = false,
+
+    -- Use symbol tree for folding. Set to true or false to enable/disable
+    -- Set to "auto" to manage folds if your previous foldmethod was 'manual'
+    -- This can be a filetype map (see :help aerial-filetype-map)
+    manage_folds = false,
+
+    -- When you fold code with za, zo, or zc, update the aerial tree as well.
+    -- Only works when manage_folds = true
+    link_folds_to_tree = false,
+
+    -- Fold code when you open/collapse symbols in the tree.
+    -- Only works when manage_folds = true
+    link_tree_to_folds = true,
+
+    -- Set default symbol icons to use patched font icons (see https://www.nerdfonts.com/)
+    -- "auto" will set it to true if nvim-web-devicons or lspkind-nvim is installed.
+    nerd_font = "auto",
+
+    -- Automatically open aerial when entering supported buffers.
+    -- This can be a function (see :help aerial-open-automatic)
+    open_automatic = false,
+
+    -- Run this command after jumping to a symbol (false will disable)
+    post_jump_cmd = "normal! zz",
+
+    -- When true, aerial will automatically close after jumping to a symbol
+    close_on_select = false,
+
+    -- The autocmds that trigger symbols update (not used for LSP backend)
+    update_events = "TextChanged,InsertLeave",
+
     guides = {
         mid_item = "├ ",
         last_item = "└ ",
         nested_top = "│ ",
         whitespace = "  ",
     },
+
+    -- Call this function when aerial first sets symbols on a buffer.
+    on_first_symbols = function(bufnr) end,
+
+    -- Call this function when aerial attaches to a buffer.
     on_attach = function(bufnr)
         -- Toggle Aerial
         vim.keymap.set("n", "<leader>aa", "<CMD>AerialToggle!<CR>", { noremap = true })
@@ -28,6 +88,80 @@ local config = {
         vim.keymap.set("n", "[Y", "<cmd>AerialPrevUp<cr>", { buffer = bufnr, desc = "Previous and Up in Aerial" })
         vim.keymap.set("n", "]Y", "<cmd>AerialNextUp<cr>", { buffer = bufnr, desc = "Next and Up in Aerial" })
     end,
+
+    -- Keymaps in aerial window. Can be any value that `vim.keymap.set` accepts.
+    -- Additionally, if it is a string that matches "aerial.<name>",
+    -- it will use the function at require("aerial.action").<name>
+    -- Set to `false` to remove a keymap
+    keymaps = {
+        ["?"] = "actions.show_help",
+        ["g?"] = "actions.show_help",
+        ["<CR>"] = "actions.jump",
+        ["<2-LeftMouse>"] = "actions.jump",
+        ["<C-v>"] = "actions.jump_vsplit",
+        ["<C-s>"] = "actions.jump_split",
+        ["p"] = "actions.scroll",
+        ["<C-j>"] = "actions.down_and_scroll",
+        ["<C-k>"] = "actions.up_and_scroll",
+        ["{"] = "actions.prev",
+        ["}"] = "actions.next",
+        ["[["] = "actions.prev_up",
+        ["]]"] = "actions.next_up",
+        ["q"] = "actions.close",
+        ["o"] = "actions.tree_toggle",
+        ["za"] = "actions.tree_toggle",
+        ["O"] = "actions.tree_toggle_recursive",
+        ["zA"] = "actions.tree_toggle_recursive",
+        ["l"] = "actions.tree_open",
+        ["zo"] = "actions.tree_open",
+        ["L"] = "actions.tree_open_recursive",
+        ["zO"] = "actions.tree_open_recursive",
+        ["h"] = "actions.tree_close",
+        ["zc"] = "actions.tree_close",
+        ["H"] = "actions.tree_close_recursive",
+        ["zC"] = "actions.tree_close_recursive",
+        ["zr"] = "actions.tree_increase_fold_level",
+        ["zR"] = "actions.tree_open_all",
+        ["zm"] = "actions.tree_decrease_fold_level",
+        ["zM"] = "actions.tree_close_all",
+        ["zx"] = "actions.tree_sync_folds",
+        ["zX"] = "actions.tree_sync_folds",
+    },
+
+    -- Options for opening aerial in a floating win
+    float = {
+        -- Controls border appearance. Passed to nvim_open_win
+        border = "rounded",
+
+        -- Determines location of floating window
+        --   cursor - Opens float on top of the cursor
+        --   editor - Opens float centered in the editor
+        --   win    - Opens float centered in the window
+        relative = "cursor",
+
+        -- These control the height of the floating window.
+        -- They can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
+        -- min_height and max_height can be a list of mixed types.
+        -- min_height = {8, 0.1} means "the greater of 8 rows or 10% of total"
+        max_height = 0.9,
+        height = nil,
+        min_height = { 8, 0.1 },
+
+        override = function(conf, source_winid)
+          -- This is the config that will be passed to nvim_open_win.
+          -- Change values here to customize the layout
+          return conf
+        end,
+    },
+
+    lsp = {
+        -- Fetch document symbols when LSP diagnostics update.
+        -- If false, will update on buffer changes.
+        diagnostics_trigger_update = true,
+
+        -- Set to false to not update the symbols when there are LSP errors
+        update_when_errors = true,
+    },
 }
 
 aeriel.setup(config)
